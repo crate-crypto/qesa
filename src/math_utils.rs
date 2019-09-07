191e2e98bbce546bbeb561769ebd56cae65a1eed
Ken Shamir <shamirken@gmail.com>
2019-09-07 14:34:21 +0100

    maths_utils:
    - move vandemonde_challenge function to math_utils

--- src/math_utils.rs
+++ src/math_utils.rs
@@ -15,3 +15,25 @@ pub fn helper_dot_product(n: usize) -> (Vec<Scalar>, Vec<Scalar>, Scalar) {
     let t: Scalar = inner_product(&a, &b);
     (a, b, t)
 }
+
+// Creates a vector from the scalar `x`
+// contents of vector = <x, x^2, x^3,.., x^n>
+// XXX: double check that it is fine to use a vandermonde matrix in regards to testing distributions to
+// expand challenges instead of fetching each challenge from the distribution
+// so we don't need `n` different challenges
+pub fn vandemonde_challenge(x: Scalar, n: usize) -> Vec<Scalar> {
+    let mut challenges: Vec<Scalar> = Vec::with_capacity(n);
+
+    let mut x_n = x.clone();
+
+    challenges.push(x_n);
+
+    for _ in 1..n {
+        x_n = x_n * x_n;
+        challenges.push(x_n)
+    }
+
+    assert_eq!(challenges.len(), n);
+
+    challenges
+}

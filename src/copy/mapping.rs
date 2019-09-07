2ad621d7fce7f46cd8a7240bf075218b178db7da
Ken Shamir <shamirken@gmail.com>
2019-09-07 20:58:01 +0100

    Mapping:
    - Refactored field names for the mapping to be clearer
    
    - Added explanation at top of file explaining the field names and the purpose
    
    - Removed explanation from middle of compute_mapping
    
    - Refactored commments in compute_mapping to be clearer or match the new field names
    
    - Corrected typo in test_random_compute_mapping

--- src/copy/mapping.rs
+++ src/copy/mapping.rs
@@ -1,8 +1,32 @@
+#![allow(non_snake_case)]
 use curve25519_dalek::{ristretto::RistrettoPoint, scalar::Scalar, traits::VartimeMultiscalarMul};
 use std::collections::BTreeSet;
-//XXX: This file in particular should be cleaned up, as first pass is very rough.
-// XXX: Is a u16 good enough as the type for the mapping? For: sha256 was 26k, can we expect circuits to be bigger than sha256? Probably not.
-//XXX: We can simplify this code, maybe we can compute the mapping more efficiently or in a more readable manner using iterators.
+
+/// The purpose of the mapping code is to map each message into the witness vector
+/// 
+/// What is a message?
+/// A message is an element in a Pedersen commitment
+/// For example, if P = aG + bH , then we have 2 messages `a` and `b`
+/// 
+/// In particular, we want to store three pieces of meta-data about a message; the commmitment_index, the position_in_witness and the commitment_crs_id
+/// 
+/// The `commitment_index` is the indice of the point that was used to commit that message
+/// For example, Imagine we have a vector of points that we use to perform a Pedersen commitment `G`
+/// We can commit to a commitment using P = a * G[0] + b * G[1] + c * G[2]
+/// In this example, we have three message `a` , `b` and `c`. 
+/// The commitment_index for `a` is `0` because we used the point at the 0th index from the vector of points `G`
+/// 
+/// The `position_in_witness` is a value that is computed `compute_mapping` 
+/// It refers to an available position in the witness that this message can be placed into
+/// This file only computes the mapping, Qesa_Copy places the messages in the relevant positions
+/// 
+/// The `commitment_crs_id` is the id of the set that was used to commit the message
+/// In this proving system, one can commit using a continuos sequence of points: P = a * G[0] + b * G[1] + c * G[2]
+/// Or you can commit to a combination of points: P = a * G[3] + b * G[5] + c * G[2] + d * G[4]
+/// Therefore, when we commit messages we also need to specify what set of points are used to commit the message
+/// This set is what we call a `commitment_crs` . In the above example, the commitment_crs would be {3,5,2,4}
+/// The `commitment_crs_id` is number that refers to a specific `commitment_crs`. In particular, the `commitment_crs_id` for a specific message
+/// is the id of the commitment_crs that was used to commit that message.
 
 pub fn commit(
     G_Vec: &[RistrettoPoint],
@@ -25,24 +49,17 @@ pub fn commit(
 pub struct Mapping {
     pub(crate) num_commitments: usize,
     pub(crate) msgs_map: Vec<MessageMap>,
-    pub(crate) unique_key_indices: BTreeSet<u16>,
+    pub(crate) unique_commitment_indices: BTreeSet<u16>,
     pub(crate) last_index: u16,
 }
 
 #[derive(Debug, Clone)]
 // Stores the relevant information about a specific message
-// `key_id` refers to the indice of the point which committed the message
-// If P = m * G[2] then key_id = 2
-// crs_id; a commitment can have multiple messages. For example P = m_1 * G[1] + m_2 * G[2]
-// The above commitment has two key_ids. The set of key_ids is called the commitmentCRS. We use crs for short.
-// Each commitment has a crs. Both the verifier and the prover share a view of the set of crs's or the set of indices.
-// The crs_id refers to the position of the specific `crs` that was used for a message in a commitment.
-// position refers to the position of the message in the witness. This is computed in `compute_mapping`
 pub struct MessageMap {
     pub(crate) msg_id: usize,
-    pub(crate) key_id: u16,
-    pub(crate) crs_id: u16,
-    pub(crate) position: u16,
+    pub(crate) commitment_index: u16,
+    pub(crate) commitment_crs_id: u16,
+    pub(crate) position_in_witness: u16,
 }
 
 pub fn compute_mapping(
@@ -50,6 +67,7 @@ pub fn compute_mapping(
     witness_size: usize,
     sets_of_indices: &[BTreeSet<u16>],
 ) -> Mapping {
+
     let n = G_Vec.len() as u16;
 
     let num_commitments = sets_of_indices.len();
@@ -60,108 +78,76 @@ pub fn compute_mapping(
         .map(|indices| indices.clone())
         .flatten()
         .collect();
-    //XXX: assert unique_indices[i] cannot be equal to zero, as reserved for witness element (see paper)?
+    
+    // The first element in the witness is reserved and so users cannot commit to it.
+    unique_indices.contains(&0);
 
-    // This is the amount of messages that are being committed to
-    // For example, if a commitment = aG+bH+cQ ; the total for this commitment would be 3
-    // Since (a,b,c) is the 3-tuple that is being committed to
+    // This is the total amount of messages that are being committed to
+    // Which is equal to the total amount of indices
     let total_messages: usize = sets_of_indices.iter().map(|indices| indices.len()).sum();
 
     let position = witness_size as u16;
     // These are the available positions in the witness vector for the messages
-    // We filter out the generator indices as they are `reserved` positions in the witness (see paper)
+    // We filter out the indices of points that are being used to commit the messages as they are `reserved` positions in the witness (see paper)
     let available_positions: Vec<u16> = (position..n - 2)
         .filter(|x| !unique_indices.contains(&x))
         .take(total_messages)
         .map(|x| x)
         .collect();
-
     let last_index = *available_positions.last().unwrap();
 
-    // This checks if there are more messages than available positions
+    // This checks if there are more messages than available positions in the witness
     // The take() method will `take` as many items as it can
     // If there is not enough, it will not panic
     // Hence, the following assert method.
     assert_eq!(total_messages, available_positions.len());
 
-    // In order to perform Qesa_Copy, we need various information
-    // This information, will be stored in a collection of maps.
-    //
-    // The first map, we will need is a map from each message to an available position in the witness - map(a)
-    // To visualise simple example:
-    // Imagine we had two commitments: P_1 = a * G[1] + b * G[2] and P_2 = c * G[3] + d * G[4]
-    // We assign each committed item a id
-    // a = 1 , b = 2, c = 3, d = 4
-    // We then assign each id to an available position
-    // `a` would go into the first available position
-    // `b` would go in the second available position
-    // `c` would go in the third available position
-    // `d` would go in the fourth available position
-    // Note that the total_messages = 4 in this case
-    //
-    // Another mapping we need is the mapping from a message to it's assosciated point - map(b)
-    // For example:
-    // Looking solely at P_1 = a * G[1] + b * G[2]
-    // the message `a` would map to G[1]
-    // the message `b` would map to G[2]
-    // We may refer to this assosciated point as the `key_id`
-    // the `key_id` refers to the position that the point is in the vector of points; G[1] has a key_id of 1.
-    //
-    //
-    // Another mapping we need is the mapping from a message to the assosciated set_of_indices
-    // For example:
-    // When we `commit()` we need to supply the indices that we want to use to commit the messages
-    // Looking at P_1 = a * G[1] + b * G[2]
-    // The commit method would look something like commit(msgs:[a,b], indices: [1,2], point_vector: G_Vec)
-    // As you can see, each commitment, has an assosciated set of indices. We want to create a map for this.
-    // We assume that the prover and verifier share a common ordered view of the set of indices, or this mapping will be off.
-    // For a protocol like rangeproof; The set of indices will always be the same, so we do not need to send this.
-    // I believe the general use-case will be similar to rangeproofs and so sending the set of indices, will be special.
-
     // Create all message maps and assign each message map an ID
-    // This ID corresponds to a specific message and depends on what order the commitments are in
+    // ID = 0 corresponds to the first message, ID = 1 corresponds to the second message and so on.
+    // The first message will the the first element from the commitment, which depends on the commitment
+    // Therefore, permuting the commitment after a mapping will fail verification
     let mut msgs_map: Vec<MessageMap> = (0..total_messages)
         .map(|i| MessageMap {
             msg_id: i,
-            key_id: 0,
-            crs_id: 0,
-            position: 0,
+            commitment_index: 0,
+            commitment_crs_id: 0,
+            position_in_witness: 0,
         })
         .collect();
 
-    // Give each message an available position in the witness vector - map(a)
+    // Map each message to an available position in the witness vector
     for (msg_map, av_pos) in msgs_map.iter_mut().zip(available_positions.iter()) {
-        msg_map.position = *av_pos;
+        msg_map.position_in_witness = *av_pos;
     }
 
-    // Now we map each msg to it's assosciated point - map(b)
+    // Map each msg to it's assosciated commitment index. 
+    // Recall this is the index of the point which is used to commit the message in the commmitment
     let mut msgs_map_iter = msgs_map.iter_mut();
     for set_of_indices in sets_of_indices.iter() {
-        for indice in set_of_indices {
-            msgs_map_iter.next().unwrap().key_id = *indice;
+        for index in set_of_indices {
+            msgs_map_iter.next().unwrap().commitment_index = *index;
         }
     }
 
-    // Now we map the messages to their assosciated set of indices - map(c)
+    // Map the messages to it's assosciated commitment_crs_id
     // XXX: We can combine this into the above for loop, for commit history we commit like this first as it's a bit clearer
     let mut msgs_map_iter = msgs_map.iter_mut();
     for (crs_id, set_of_indices) in sets_of_indices.iter().enumerate() {
         for _ in set_of_indices {
-            msgs_map_iter.next().unwrap().crs_id = crs_id as u16;
+            msgs_map_iter.next().unwrap().commitment_crs_id = crs_id as u16;
         }
     }
 
     Mapping {
         num_commitments: num_commitments,
         msgs_map: msgs_map,
-        unique_key_indices: unique_indices,
+        unique_commitment_indices: unique_indices,
         last_index: last_index,
     }
 }
 
 #[test]
 fn test_random_compute_mapping() {
-    use super::*;
     use rand::Rng;
     let mut rng = rand::thread_rng();
 
@@ -170,7 +156,7 @@ fn test_random_compute_mapping() {
 
     let G: Vec<RistrettoPoint> = (0..n).map(|_| RistrettoPoint::random(&mut rng)).collect();
 
-    // We will hae four commitments and so four sets of indices
+    // We will have four commitments and so four sets of indices
     let sets_of_indices: Vec<BTreeSet<u16>> = (0..num_of_commitments)
         .map(|_| {
             let mut set = BTreeSet::new();
@@ -186,6 +172,7 @@ fn test_random_compute_mapping() {
             set
         })
         .collect();
+
     // The above code tells us that we are committing to (num_of_commitments * num_of_indices) elements
     // In this case we are committing to max 8 elements,if each element is commmited to with a different indice, then
     // we have 8 x 2 = 16 reserved spots in the witness for the generator points, and 2 spots reserved for the randomness.
@@ -205,7 +192,7 @@ fn test_random_compute_mapping() {
 
     // Check that the last index corresponds to the position of the last message
     let last_msg_id = total_messages - 1;
-    assert_eq!(map.msgs_map[last_msg_id].position, map.last_index);
+    assert_eq!(map.msgs_map[last_msg_id].position_in_witness, map.last_index);
 
     // There should only be 4 crs_ids from 0..3 since we only have four commitments
     // They should also be ordered by msg_id and ascending
@@ -216,16 +203,17 @@ fn test_random_compute_mapping() {
     let chunked_maps: Vec<_> = sets_of_indices
         .iter()
         .enumerate()
-        .map(|(i, set_of_indice)| {
+        .map(|(_, set_of_indice)| {
             let num_of_indice = set_of_indice.len();
             let (head, tail) = map_msg.split_at(num_of_indice);
             map_msg = tail;
             head
         })
         .collect();
+
     for (crs_id, maps) in chunked_maps.iter().enumerate() {
         for map in maps.iter() {
-            assert!(map.crs_id == crs_id as u16)
+            assert!(map.commitment_crs_id == crs_id as u16)
         }
     }
 }

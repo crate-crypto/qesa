e0cf3699bcde7fb42a4d54e101546675f68f9cce
Ken Shamir <shamirken@gmail.com>
2019-09-07 21:33:23 +0100

    Copy:
    - Refactor explanation at top of file, for purpose of Qesa_Copy
    
    - In create(), remove mut label from G_Vec
    
    - In create(), change field name `commitments` to `openings`
    
    - Refactor comments explaining the process of proof creation and verification

--- src/copy/copy.rs
+++ src/copy/copy.rs
@@ -10,49 +10,54 @@ use curve25519_dalek::{
     traits::VartimeMultiscalarMul,
 };
 use merlin::Transcript;
-// This module is in charge of allowing qesa to take commitments
-// as input to the qesa_inner/zk protocol
+/// Qesa_Zk alone is used to ensure quadratic eqaution satisfiability
+/// It cannot provide any guarantees regarding external input such as Pedersen Commitments
+/// Qesa_Copy acts as a layer on top to allow us to `map` commitments onto the witness
+/// Which we can then use in Qesa_ZK
+/// 
 #[derive(Clone)]
 pub struct Copy {
-    c_w: CompressedRistretto,
+    C_w: CompressedRistretto,
     inner: qesa_inner::Inner,
 }
+#[allow(dead_code)]
 pub fn create(
     transcript: &mut Transcript,
-    mut G_Vec: Vec<RistrettoPoint>,
+    G_Vec: Vec<RistrettoPoint>,
     H_Vec: Vec<RistrettoPoint>,
     Q: &RistrettoPoint,
     map: Mapping,
     mut gamma_i: BlockMatrix,
     mut w: Vec<Scalar>,
-    commitments: Vec<(Vec<Scalar>, Scalar)>, // (messages, rand)
+    openings: Vec<(Vec<Scalar>, Scalar)>, // (messages, rand)
 ) -> Copy {
-    // 1. Setup Witness
-    //
-    // number of commitments stated in the map
-    // should be equal to the number of commitments passed as an argument
-    assert_eq!(map.num_commitments, commitments.len());
-
-    // first element in the witness should be equal to one (see paper)
-    assert_eq!(w[0], Scalar::one());
 
     let n = G_Vec.len();
 
-    // Pad the witness with zeroes
+    assert_eq!(map.num_commitments, openings.len());
+
+    // Ensure the first element in the witness is `1` (see paper)
+    assert_eq!(w[0], Scalar::one());
+
+
+    // 1. Pad the witness with zeroes
+    //
     let pad_by = (n - 2) - (w.len());
     let zeroes: Vec<Scalar> = (0..pad_by).map(|_| Scalar::zero()).collect();
     w.extend(zeroes);
 
-    // Create extended witness with two random values
+    // 2. Compute r'
+    //
     let mut rng = rand::thread_rng();
     let r_prime: Vec<Scalar> = (0..2).map(|_| Scalar::random(&mut rng)).collect();
+    
+    // 3. Compute w' (extended witness) using r'
+    //
     let mut w_prime = [&w[..], &r_prime[..]].concat();
 
-    // Use the map to place the messages in the correct position
-    // in the witness
+    //4. Use the map to place the messages in the correct position in the witness
     //
-    // Collect all messages
-    let all_messages: Vec<Scalar> = commitments
+    let all_messages: Vec<Scalar> = openings
         .iter()
         .map(|(msgs, _)| msgs)
         .flatten()
@@ -62,21 +67,23 @@ pub fn create(
         w_prime[msg_map.position_in_witness as usize] = *msg;
     }
 
-    // 2. Compute commitment to witness
+    // 5. Compute commitment (C_w) to witness
     //
-    let c_w = RistrettoPoint::vartime_multiscalar_mul(w_prime.iter(), G_Vec.iter());
-    transcript.append_message(b"c_w", c_w.compress().as_bytes());
+    let C_w = RistrettoPoint::vartime_multiscalar_mul(w_prime.iter(), G_Vec.iter());
+    transcript.append_message(b"c_w", C_w.compress().as_bytes());
 
-    // 3. Use challenge to adjust the witness value
+    // 6. Use challenges to adjust the witness value
     //
     let alpha = transcript.challenge_scalar(b"alpha");
-    let alpha_challenges = vandemonde_challenge(alpha, commitments.len());
+    let alpha_challenges = vandemonde_challenge(alpha, openings.len());
 
-    // Compute aggregated random value
+    // 7.Compute aggregated random value
     //
     // Collect all of the random values from the openings
-    let all_rand_openings: Vec<Scalar> = commitments.iter().map(|(_, rnd)| *rnd).collect();
-    // use challenge to aggregate all values
+    let all_rand_openings: Vec<Scalar> = openings.iter().map(|(_, rnd)| *rnd).collect();
+    //
+    // Use challenges to aggregate all random values
+    //
     let aggregated_rand: Scalar = all_rand_openings
         .iter()
         .zip(alpha_challenges.iter())
@@ -84,49 +91,58 @@ pub fn create(
         .sum();
     w_prime[n - 1] += aggregated_rand;
 
-    // Compute aggregated witness values
-    // XXX: This needs cleaning up and more commenting, so that readers know what is happening
+    // 8. Compute aggregated witness values
+    //
     for msg_map in map.msgs_map.iter() {
-        // For each message. We need to figure out what position
-        // it is in, in terms of it's original commitment
-        // This is the same as the key_id or generator indice (see paper - orange values)
-        let key_id_position = msg_map.commitment_index;
-        // This position is it's usable position in the witness vector. (see paper - green values)
+        // For each message. We need to figure out it's commitment index
+        // This is the same as the generator indice (see paper - orange values)
+        let commitment_index = msg_map.commitment_index;
+        // This position is it's actual position in the witness vector. (see paper - green values)
         let msg_position = msg_map.position_in_witness;
         // For each row of messages, the challenge value is the same
-        // In order to determine what challenge is needed
-        // We can fetch the crs_id
-        let crs_id = msg_map.commitment_crs_id;
+        // In other words, for each commitment `P` the challenge is the same for all messages (see paper)
+        // In order to determine what challenge is needed, we can fetch the commitment_crs_id
+        // commitment_crs_id = 0 corresponds to the first commitment and therefore the first challenge 
+        let commitment_crs_id = msg_map.commitment_crs_id;
 
-        let w_msg_pos = w_prime[msg_position as usize];
+        let msg = w_prime[msg_position as usize];
+        let alpha = alpha_challenges[commitment_crs_id as usize];
 
-        w_prime[key_id_position as usize] += w_msg_pos * alpha_challenges[crs_id as usize];
+        w_prime[commitment_index as usize] += msg * alpha;
     }
 
-    // 4. Modify gamma to match the new commited values
-    // Make sure they are "correct" - expand on this
-    //
+    // 9. Add `copy matrices` to make sure that the verifier
+    // can check that the aggregated values are equal to the aggregated committed values
+    // Essentially, we add constraints/equations, so that the prover cannot use values that he has not commited to
+    // 
+    // Since we are using dense matrices, we will need to add padding
     let zeroes: Vec<Scalar> = (0..n - 2).map(|_| Scalar::zero()).collect();
 
-    for com_index in map.unique_commitment_indices {
+    for commitment_index in map.unique_commitment_indices {
+       
         let mut matrix: Vec<Vec<Scalar>> = Vec::new();
+       
         let mut new_eqn: Vec<Scalar> = (0..n - 2).map(|_| Scalar::zero()).collect();
-        new_eqn[com_index as usize] = -Scalar::one();
+        new_eqn[commitment_index as usize] = -Scalar::one();
 
         // Get all messages for this commitment index
         let msgs_for_comm_index: Vec<&MessageMap> = map
             .msgs_map
             .iter()
-            .filter(|x| (x.commitment_index == com_index))
+            .filter(|x| (x.commitment_index == commitment_index))
             .collect();
 
         for msg in msgs_for_comm_index.iter() {
-            new_eqn[msg.position_in_witness as usize] = alpha_challenges[msg.commitment_crs_id as usize];
+            
+            let alpha = alpha_challenges[msg.commitment_crs_id as usize];
+
+            new_eqn[msg.position_in_witness as usize] = alpha;
         }
 
-        // Add to matrix
+        // Add equation to matrix
         matrix.push(new_eqn);
-        // Add dummy lines, to make the matrix n-2 x n-2
+
+        // Add dummy equations, to make the matrix n-2 x n-2
         // XXX: Once sparse matrices are implemented, we can remove this.
         while matrix.len() != n - 2 {
             matrix.push(zeroes.clone());
@@ -139,13 +155,12 @@ pub fn create(
     // Decompose the extended witness by splitting vector back into r_prime and w_prime
     // Note that w_prime and r_prime are different than the original w and r_prime
     let r_prime = w_prime.split_off(n - 2);
-    assert_eq!(r_prime.len(), 2);
-    assert_eq!(w_prime.len(), n - 2);
-    //5. Run Qesa_inner as a sub protocol
+
+    //5. Call Qesa_Inner as a sub-protocol
     //
     let proof = qesa_inner::create(transcript, G_Vec, H_Vec, Q, &gamma_i, w_prime, r_prime);
     Copy {
-        c_w: c_w.compress(),
+        C_w: C_w.compress(),
         inner: proof,
     }
 }
@@ -160,12 +175,15 @@ impl Copy {
         map: Mapping,
         commitments: Vec<RistrettoPoint>,
     ) -> bool {
+        
         let n = G_Vec.len();
 
         //1. Add commited witness to the transcript
-        transcript.append_message(b"c_w", self.c_w.as_bytes());
+        //
+        transcript.append_message(b"c_w", self.C_w.as_bytes());
 
         // 2. Use challenge to adjust the commitment values
+        //
         let alpha = transcript.challenge_scalar(b"alpha");
         let alpha_challenges = vandemonde_challenge(alpha, commitments.len());
 
@@ -174,9 +192,10 @@ impl Copy {
             .zip(commitments.iter())
             .map(|(alpha, commitment)| commitment * alpha)
             .sum();
-        commitment = commitment + self.c_w.decompress().unwrap();
+        commitment = commitment + self.C_w.decompress().unwrap();
 
-        // 4. Modify gamma to match the new commited values
+        // 3. Modify gamma to match the new commited values
+        //
         let zeroes: Vec<Scalar> = (0..n - 2).map(|_| Scalar::zero()).collect();
 
         for key_id in map.unique_commitment_indices {
@@ -192,27 +211,32 @@ impl Copy {
                 .collect();
 
             for msg in msgs_for_comm_index.iter() {
-                new_eqn[msg.position_in_witness as usize] = alpha_challenges[msg.commitment_crs_id as usize];
+                
+                let alpha = alpha_challenges[msg.commitment_crs_id as usize];
+
+                new_eqn[msg.position_in_witness as usize] = alpha;
             }
 
-            // Add to matrix
+            // Add equation to matrix
             matrix.push(new_eqn);
-            // Add dummy lines, to make the matrix n-2 x n-2
-            // Once sparse matrices are implemented, we can remove this.
+
+            // Add dummy equations, to make the matrix n-2 x n-2
             while matrix.len() != n - 2 {
                 matrix.push(zeroes.clone());
             }
+
             // Add matrix to block matrix
             gamma_i.push(matrix);
         }
+
+        //4. Call Qesa_Inner as a sub-protocol using the modified commitment
+        //
         let proof_inner = qesa_inner::Inner {
             alm_zk: self.inner.alm_zk.clone(),
             c_prime_w: commitment.compress(),
             c_prime_prime_w: self.inner.c_prime_prime_w,
         };
-
         proof_inner.verify(transcript, G_Vec, H_Vec, Q, &gamma_i)
-        // self.inner.verify(transcript, G_Vec, H_Vec, Q, &gamma_i)
     }
 }
 #[cfg(test)]
@@ -279,8 +303,8 @@ mod tests {
             })
             .collect();
 
-        let map = mapping::compute_mapping(&G, witness.len(), &sets_of_indices);
-        let mut transcript = Transcript::new(b"copy");
+        let map = mapping::compute_mapping(G.len(), witness.len(), &sets_of_indices);
+        let mut prover_transcript = Transcript::new(b"copy");
 
         let openings = messages
             .into_iter()
@@ -289,7 +313,7 @@ mod tests {
             .collect();
 
         let proof = create(
-            &mut transcript,
+            &mut prover_transcript,
             G.clone(),
             H.clone(),
             &Q,
@@ -300,9 +324,9 @@ mod tests {
         );
 
         // Verify
-        let mut transcript = Transcript::new(b"copy");
+        let mut verifier_transcript = Transcript::new(b"copy");
         let bm = BlockMatrix::new();
-        assert!(proof.verify(&mut transcript, G, H, &Q, bm, map.clone(), commitments));
+        assert!(proof.verify(&mut verifier_transcript, G, H, &Q, bm, map.clone(), commitments));
     }
     #[test]
     fn test_copy_proof_creation_single_commit() {
@@ -337,7 +361,7 @@ mod tests {
         let commitment = mapping::commit(&G, &set_of_indice[0], &message, &rand);
         let commitments: Vec<RistrettoPoint> = vec![commitment];
 
-        let map = mapping::compute_mapping(&G, witness.len(), &set_of_indice);
+        let map = mapping::compute_mapping(G.len(), witness.len(), &set_of_indice);
         let mut transcript = Transcript::new(b"copy");
 
         let openings: Vec<(Vec<Scalar>, Scalar)> = vec![(message, rand)];
